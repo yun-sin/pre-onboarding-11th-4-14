83d23c2b792ff1c875a96bc77005d967a9aaa008
장윤신 <99275134+yun-sin@users.noreply.github.com>
2023-07-17 22:34:48 +0900

    Feat: 입력마다 API를 호출하지 않도록 타이머 설정

--- src/hooks/useSickAxios.ts
+++ src/hooks/useSickAxios.ts
@@ -3,15 +3,20 @@ import { getSick } from '../apis/SickAxios';
 
 export function useSickAxios() {
   const [searchResult, setSearchResult] = useState<string[]>(['검색어 없음']);
+  const [timerId, setTimerId] = useState<number>();
 
-  const onInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    clearTimeout(timerId); // 이전의 타이머를 취소
+
     const inputText = e.target.value.trim();
 
     if (inputText !== '') {
       setSearchResult(['검색중...']);
-
-      const data = await getSick(inputText); // 500ms 지연 후 API 호출
-      if (data) setSearchResult(data);
+      const id = setTimeout(async () => {
+        const data = await getSick(inputText); // 500ms 지연 후 API 호출
+        if (data) setSearchResult(data);
+      }, 500);
+      setTimerId(Number(id));
     } else {
       setSearchResult(['검색어 없음']);
     }
